51cbe286e07e8e816dc4cbdd42f13aaabb4b3f9a
Lewis Capers <lewiscapersjr206@gmail.com>
2021-08-04 21:19:27 -0700

    script.js functions added

--- develop/script.js
+++ develop/script.js
@@ -12,6 +12,47 @@ var numberSelect = false;
 
 var generateBtn = document.querySelector("#generate");
 
+function generate() {
+  var confirmLength = '';
+  while (isNaN(confirmLength) || confirmLength <8 || confirmLength > 128) {
+    confirmLength = prompt("How long would you like your password to be? (Must be between 8-128)");
+    if(confirmLength === null) {
+      break;
+    }
+  }
+
+  if (confirmLength) {
+    if (confirm("Would you like to use uppercase letters?") == true) {
+      capSelect = true
+    }
+
+    if (confirm("Would you like to use lowercase letters?") == true) {
+      letterSelect = true
+    }
+
+    if (confirm("Would you like to use special characters?") == true) {
+      specialSelect = true
+    }
+
+    if (confirm("Would you like to use numbers?") == true) {
+      numberSelect = true
+    }
+
+    if (capSelect === false && letterSelect === false && specialSelect === false && numberSelect === false) {
+      alert("YOU HAVE TO SELECT AT LEAST ONE CHARACTER TYPE PLEASE!")
+    }
+  }
+
+  var characters = '';
+  characters += (letterSelect ? lower : '');
+  characters += (capSelect ? upper : '');
+  characters += (specialSelect ? special : '');
+  characters += (numberSelect ? number : '');
+
+  pwd = password(confirmLength, characters);
+
+  
+}
 
 
 
